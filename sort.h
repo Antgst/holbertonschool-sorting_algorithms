35195b36d5bb9f06595a9b6282bfa0abbd6a2151
Antgst <ant.gousset@gmail.com>
2025-12-10 22:42:41 +0100

    add: add T1

--- sort.h
+++ sort.h
@@ -10,6 +10,15 @@
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
+ * 
+ * Please, note this format is used for Quiz and Task questions.
+ * O(1)
+ * O(n)
+ * O(n!)
+ * n square -> O(n^2)
+ * log(n) -> O(log(n))
+ * n * log(n) -> O(nlog(n))
+ * n + k -> O(n+k)
  */
 
 typedef struct listint_s
