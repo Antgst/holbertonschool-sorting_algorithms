ec645c8169237686de565025e983b6b1f614552e
Antgst <ant.gousset@gmail.com>
2025-12-10 22:55:35 +0100

    add: add structure of T1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order 
+ * using the insertion sort algoritm
+ * @list: pointer to a pointer to the head of the list
+ * 
+ * Return: Print the list after each swap
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+
+}
