a0609e4b896f33f89f87a86207a99fa3cbad932c
Antgst <ant.gousset@gmail.com>
2025-12-10 22:58:06 +0100

    add: add the structure of T2

---
+++ 2-selection_sort.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order
+ * using the selection sort algoritm
+ * @array: the array to be sorted
+ * @size: the number of elements in the array
+ * 
+ * Return: The array is sorted
+ */
+
+void selection_sort(int *array, size_t size)
+{
+
+}
