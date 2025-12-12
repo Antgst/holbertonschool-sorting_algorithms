2398a4f14ad7f0947d46a650c7fb163f525bb228
Antgst <ant.gousset@gmail.com>
2025-12-12 13:51:32 +0100

    fix: edit betty issues on T1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,10 +1,10 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending order 
- * using the insertion sort algoritm
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ * in ascending order using the insertion sort algoritm
  * @list: pointer to a pointer to the head of the list
- * 
+ *
  * Return: Print the list after each swap
  */
 
