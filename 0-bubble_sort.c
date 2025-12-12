52d88e9678db55dfed921dd0542dc7c40fd77bce
Antgst <ant.gousset@gmail.com>
2025-12-12 13:50:31 +0100

    fix: edit betty issues T0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,10 +1,11 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sort and array of integers in ascending order using the Bubble sort algorithm
+ * bubble_sort - Sort and array of integers in ascending order
+ * using the Bubble sort algorithm
  * @array: The array that have to be sorted
  * @size: Number of elements in the array
- * 
+ *
  * Return: The array is sorted
  */
 
@@ -16,20 +17,20 @@ void bubble_sort(int *array, size_t size)
 
 	if (array == NULL && size < 2)
 		return;
-	
+
 	swp = 1;
 
 	while (swp)
 	{
 		swp = 0;
 
-		for (i = 0; i < size -1; i++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[i] > array [i + 1])
+			if (array[i] > array[i + 1])
 			{
-				tmp = array [i];
-				array [i] = array [i + 1];
-				array [i + 1] = tmp;
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
 
 				print_array(array, size);
 				swp = 1;
