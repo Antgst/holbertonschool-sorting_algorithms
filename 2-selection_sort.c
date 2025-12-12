99e4515112a79612ff5be22a6fe4c621e533d9f6
GigiCorlay <11489@holbertonstudents.com>
2025-12-12 15:14:44 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,21 +12,18 @@
 */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_index;
+	size_t i, j, min_index = 0;
 	int tmp;
 
-	if (array == NULL || size < 2)
+	if (!array || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min_index = i;
-
 		for (j = i + 1; j < size; j++)
-		{
 			if (array[j] < array[min_index])
 				min_index = j;
-		}
 
 		if (min_index != i)
 		{
