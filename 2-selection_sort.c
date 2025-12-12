146408d408d0a2e80533156347813166f6201f45
GigiCorlay <11489@holbertonstudents.com>
2025-12-12 16:08:18 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -24,7 +24,6 @@ void selection_sort(int *array, size_t size)
 		for (j = i + 1; j < size; j++)
 			if (array[j] < array[min_index])
 				min_index = j;
-
 		if (min_index != i)
 		{
 			tmp = array[i];
