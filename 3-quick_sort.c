5afeab6d9ff205e32db69f67c5258bce976a76fe
Antgst <ant.gousset@gmail.com>
2025-12-12 15:46:31 +0100

    add: add T3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,102 +1,78 @@
-#include <stdio.h>
 #include "sort.h"
+void recurs(int array[], int low, int high, size_t size);
+int partition(int array[], int low, int high, size_t size);
 
 /**
- * swap - Swap two integers in an array.
- * @x: First integer to swap.
- * @y: Second integer to swap.
+ * quick_sort - Sorts an array of integers in ascending order using Quick sort.
+ * @array: Array of integers to sort.
+ * @size: Size of the array.
  *
- * Picture two kids trading Pokemon cards - they just swap them.
- * This function does exactly that but with numbers inside memory.
-**/
-void swap(int *x, int *y)
+ * Description: Uses the Lomuto partition scheme.
+ *              The pivot is always the last element.
+ *              Prints the array after each swap.
+ */
+void quick_sort(int *array, size_t size)
 {
-	int temp = *x;
-	*x = *y;
-	*y = temp;
+	if (!array || size < 2)
+		return;
+
+	recurs(array, 0, size - 1, size);
 }
 
 /**
- * lomuto - Lomuto partition scheme for quicksort.
- * @array: The array of integers to sort.
+ * partition - Partitions the array for Quick sort.
+ * @array: Array of integers to partition.
  * @low: Starting index of the partition.
  * @high: Ending index of the partition.
- * @size: Size of the full array.
+ * @size: Size of the array.
  *
- * Return: The pivot index
- *
- * This is where the magic happens  ^`^t we pick the last element as
- * the 'pivot' and rearrange the array so that:
- * all numbers smaller than or equal to it are on the left,
- * and all the bigger numbers go on the right.
-**/
-int lomuto(int *array, int low, int high, size_t size)
+ * Return: The index of the pivot after partitioning.
+ */
+int partition(int array[], int low, int high, size_t size)
 {
-	int pivot_value = array[high];
-	int i, j;
-
-	i = low - 1;
+	int pivot = array[high];
+	int temp, j;
+	int i = low;
 
 	for (j = low; j < high; j++)
 	{
-		if (array[j] <= pivot_value)
+		if (array[j] <= pivot)
 		{
-			i++;
-			if (i != j)
+			if (i < j)
 			{
-				swap(&array[i], &array[j]);
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
 				print_array(array, size);
 			}
+			i++;
 		}
 	}
-
-	i++;
 	if (i != high)
 	{
-		swap(&array[i], &array[high]);
+		temp = array[i];
+		array[i] = array[high];
+		array[high] = temp;
 		print_array(array, size);
 	}
-
 	return (i);
 }
 
 /**
- * quick_sort - Sort an array of integers in ascending order using quicksort.
- * @array: The array to sort.
- * @size: The size of the array.
- *
- * This is the 'party planner' - it sets everything up for quicksort
- * by calling the recursive function with the full array.
- * If the array is empty or has only one element, we just... do nothing.
+ * recurs - Recursively applies Quick sort to subarrays.
+ * @array: Array of integers to sort.
+ * @low: Starting index of the subarray.
+ * @high: Ending index of the subarray.
+ * @size: Size of the array.
  */
-void quick_sort(int *array, size_t size)
+void recurs(int array[], int low, int high, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
-
-	quicksort_recursion(array, 0, size - 1, size);
-}
-
-/**
- * quicksort_recursion - Recursively sort an array using quicksort.
- * @array: The array of integers to sort.
- * @low: Starting index of the current partition.
- * @high: Ending index of the current partition.
- * @size: Size of the full array.
- *
- * Think of this like cutting a cake into smaller and smaller pieces
- * until all slices are perfectly sized. We use recursion to zoom into
- * each partition until it's so small (one element) that it's already 'sorted'.
-**/
-void quicksort_recursion(int *array, int low, int high, size_t size)
-{
-	int pivot_index;
+	int index_pivot;
 
 	if (low < high)
 	{
-		pivot_index = lomuto(array, low, high, size);
-
-		quicksort_recursion(array, low, pivot_index - 1, size);
-		quicksort_recursion(array, pivot_index + 1, high, size);
+		index_pivot = partition(array, low, high, size);
+		recurs(array, low, index_pivot - 1, size);
+		recurs(array, index_pivot + 1, high, size);
 	}
 }
--- sort.h
+++ sort.h
@@ -10,22 +10,12 @@
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
- * 
- * Please, note this format is used for Quiz and Task questions.
- * O(1)
- * O(n)
- * O(n!)
- * n square -> O(n^2)
- * log(n) -> O(log(n))
- * n * log(n) -> O(nlog(n))
- * n + k -> O(n+k)
  */
-
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -36,9 +26,4 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-void swap(int *x, int *y);
-int lomuto(int *array, int low, int high, size_t size);
-void quick_sort(int *array, size_t size);
-void quicksort_recursion(int *array, int low, int high, size_t size);
-
-#endif
+#endif /* SORT_H */
