f328cd55cfa9076acb32dd34e08bbd8c56ba603f
Antgst <ant.gousset@gmail.com>
2025-12-12 14:28:00 +0100

    fix: syntax of T2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -17,28 +17,25 @@ void selection_sort(int *array, size_t size)
 	size_t j;
 	size_t low;
 	int tmp;
-
+	
 	if (array != NULL)
 		return;
-
-		for (i = 0; i < size - 1; i++)
+		
+	for (i = 0; i < size - 1; i++)
+	{
+		low = i;
+		
+		for (j = i + 1; j < size; j++)
 		{
-			low = i;
-
-			for (j = i + 1; j < size; j++)
-			{
-				if (array[j] < array[low])
-
-				low = j;
-			}
-
-			if (low != 1)
-			{
-				tmp = array[i];
-				array[i] = array[low];
-				array[low] = tmp;
-
-				print_array(array, size);
+			if (array[j] < array[low])
+			low = j;
+		}
+		if (low != 1)
+		{
+			tmp = array[i];
+			array[i] = array[low];
+			array[low] = tmp;
+			print_array(array, size);
 		}
 	}
 }
