941032040efc153289a82256f7c0bb5b28e2b6d4
GigiCorlay <11489@holbertonstudents.com>
2025-12-12 15:03:41 +0100

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,31 +3,29 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers in ascending order
- *                   using the Selection sort algorithm
- * @array: pointer to the array to sort
- * @size: size of the array
- *
- * Return: void
- */
+* selection_sort - sorts an array of integers in ascending order
+*                   using the Selection sort algorithm
+* @array: pointer to the array to sort
+* @size: size of the array
+*
+* Return: void
+*/
 void selection_sort(int *array, size_t size)
 {
-	size_t j;
+	size_t i, j, min_index;
 	int tmp;
 
-	size_t i = 0;
-	size_t min_index;
+	if (array == NULL || size < 2)
+		return;
 
-	while (i < size - 1)
+	for (i = 0; i < size - 1; i++)
 	{
 		min_index = i;
 
-		j = i + 1;
-		while (j < size)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_index])
 				min_index = j;
-			j++;
 		}
 
 		if (min_index != i)
@@ -38,7 +36,5 @@ void selection_sort(int *array, size_t size)
 
 			print_array(array, size);
 		}
-
-		i++;
 	}
 }
